e52e4a1b593e5958afe647092450e1789e05364d
Jalen Davenport <jalen@jalendport.com>
2025-07-22 17:09:02 -0400

    Update Comments integration
    Prevent Altcha from trying to validate if saving a comment in the CP

--- src/integrations/Comments.php
+++ src/integrations/Comments.php
@@ -13,21 +13,29 @@ class Comments extends Component
 	public static function beforeSaveComment(ModelEvent $event): void
 	{
 
+		/** @var Comment $comment */
 		$comment = $event->sender;
-		$payload = Craft::$app->getRequest()->post('altcha');
+		$currentScenario = $comment->scenario;
 
-		// If the payload is empty, we can't validate
-		if (empty($payload)) {
-			$event->isValid = false;
-			$comment->addError('altcha', Craft::t('altcha', 'Please complete the Altcha challenge.'));
-			return;
-		}
+		// Prevent Altcha from validating in the CP
+		if ($currentScenario === Comment::SCENARIO_FRONT_END) {
 
-		// Verify the solution
-		$verified = Altcha::getInstance()->altchaService->verifySolution($payload);
+			$payload = Craft::$app->getRequest()->post('altcha');
 
-		if (!$verified) {
-			$comment->status = Comment::STATUS_SPAM;
+			// If the payload is empty, we can't validate
+			if (empty($payload)) {
+				$event->isValid = false;
+				$comment->addError('altcha', Craft::t('altcha', 'Please complete the Altcha challenge.'));
+				return;
+			}
+
+			// Verify the solution
+			$verified = Altcha::getInstance()->altchaService->verifySolution($payload);
+
+			if (!$verified) {
+				$comment->status = Comment::STATUS_SPAM;
+			}
+
 		}
 
 	}
